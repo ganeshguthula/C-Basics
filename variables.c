2ef5b16b05fde5f1fcc22e8e18a423157f430803
ganeshguthula <92910614+ganeshguthula@users.noreply.github.com>
2021-10-28 23:49:14 -0700

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//Initialization
+	age = 20 ;
+	cgpa = 8.5 ;
+	initial = 'G';
+	
+	//output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c",initial);
+	
+}
