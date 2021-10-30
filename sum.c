e2d9608a46c31a46cc0d9297d257b2fc9d798714
ganeshguthula <92910614+ganeshguthula@users.noreply.github.com>
2021-10-30 12:32:22 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    //variables
+	int person1, person2, add;
+	int num1,num2,sum ;
+	 
+	 //Initialization
+	 person1 = 899;
+	 person2 = 1020;
+	 num1 = 2024;
+	 num2 = 5678;
+
+	 //calculation
+	 add = person1+person2;
+	 sum = num1+num2;
+	 
+	 //output
+	 printf("sum of %d and %d is %d\n",person1,person2,add); 
+	 printf("sum of %d and %d is %d",num1,num2,sum);   
+}
